1d1b71b630d1def381859828c9efcea2da4fe473
sriharsha739s <64218018+sriharsha739s@users.noreply.github.com>
2020-07-11 23:16:16 +0800

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,25 @@
+## Adding elements to different lists
+
+lst = [12, 1, 3, 4, 56, 90, 00, 99,2,3,5,5,6]
+new = []
+
+for x in lst:
+    new.append(x)
+    
+print(new)
+
+## Accessing elements from a tuple
+
+x = (1, 2, 3)
+print(type(x))
+print(x[1])
+
+## Deleting different dictionary elements
+
+
+dct = {'x': 45,'y':67,'z': 69}
+
+del dct['x']
+
+print(dct) 
+
